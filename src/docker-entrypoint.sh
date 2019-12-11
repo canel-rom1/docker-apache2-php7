94da88b2781fee991912b581c9c5dc69d0b3e4d9
rom1 <rom1@canel.ch>
2019-12-11 12:26:35 +0100

    Mail configuration in PHP with msmtp.

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -9,6 +9,16 @@ then
         source ${ENTRYPOINT}
 fi
 
+# Configuration msmtp
+MSMTPRC_FILE="/etc/msmtprc"
+[ -n "${SMTP_HOST}" ]     && sed -i "s/<HOST>/${SMTP_HOST}/"         "${MSMTPRC_FILE}"
+[ -n "${SMTP_PORT}" ]     && sed -i "s/<PORT>/${SMTP_PORT}/"         "${MSMTPRC_FILE}"
+[ -n "${SMTP_MAILFROM}" ] && sed -i "s/<MAILFROM>/${SMTP_MAILFROM}/" "${MSMTPRC_FILE}"
+[ -n "${SMTP_USER}" ]     && sed -i "s/<USER>/${SMTP_USER}/"         "${MSMTPRC_FILE}"
+[ -n "${SMTP_PASSWORD}" ] && sed -i "s/<PASSWORD>/${SMTP_PASSWORD}/" "${MSMTPRC_FILE}"
+
+PHP_INI="/etc/php7/php.ini"
+
 if [ "${1:0:1}" = "-" ] ; then
 	exec /usr/sbin/httpd "$@"
 fi
